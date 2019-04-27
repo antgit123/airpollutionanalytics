60ca710408c75cec1ca94e54cd0a28872e1b2406
Sindhu-Sekar <ssekar@student.unimelb.edu.au>
2019-04-27 16:04:27 +1000

    Adding Alias for column names

--- DataProcessing/ProcessScatFile.py
+++ DataProcessing/ProcessScatFile.py
@@ -11,14 +11,12 @@ def calcNoOfTrafficPerHr(df, concatenatedDf, trafficSite):
     result.append('NB_SCATS_SITE')
     df = df.select(result)
 
+    # Filter data within EPA sites
     df = df.join(trafficSite, trafficSite.SCAT_SITE_ID == df.NB_SCATS_SITE, 'inner')
-    # df.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").mode('append') \
-    #     .save("E:\StudyNotes\Semester4\Project\data\ppdd1")
-    # df = df.select(result)
-    #
+
     # Sum the values on hourly basis
-    i = 0;
-    count = 0;
+    i = 0
+    count = 0
     initialCount = len(df.columns) - 4
     while i < initialCount:
         count = count + 1
@@ -26,7 +24,6 @@ def calcNoOfTrafficPerHr(df, concatenatedDf, trafficSite):
                            df[df.columns[i + 0]] + df[df.columns[i + 1]] + df[df.columns[i + 2]] + df[
                                df.columns[i + 3]])
         i = i + 4
-    # df.show()
 
     # Group by NB_SCATS_SITE and sum the value
     df2 = df.groupBy("NB_SCATS_SITE").sum('sum_1', 'sum_2', 'sum_3', 'sum_4', 'sum_5', 'sum_6',
@@ -34,20 +31,9 @@ def calcNoOfTrafficPerHr(df, concatenatedDf, trafficSite):
                                           'sum_13', 'sum_14', 'sum_15', 'sum_16', 'sum_17', 'sum_18',
                                           'sum_19', 'sum_20', 'sum_21', 'sum_22', 'sum_23', 'sum_24')
 
-    df2 = df2.join(trafficSite, trafficSite.SCAT_SITE_ID == df.NB_SCATS_SITE, 'inner')
-    # # Combine wkt column from traffice file with the above dataframe
-    sumColumns = [i for i in df2.columns if i.startswith('sum(sum_')]
-    sumColumns.append('NB_SCATS_SITE')
-    sumColumns.append('SCAT_SITE_NAME')
-    sumColumns.append('SCAT_LATITUDE')
-    sumColumns.append('SCAT_LONGITUDE')
-    processedDf = df2.select(sumColumns)
     if concatenatedDf == 0:
-        concatenatedDf = processedDf
+        concatenatedDf = df2
     else:
-        concatenatedDf = concatenatedDf.union(processedDf)
+        concatenatedDf = concatenatedDf.union(df2)
 
     return concatenatedDf
-    # Display the final result
-    # finalDf.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").mode('append') \
-    #     .save(newCsvPath)
--- DataProcessing/ProcessingScatsFromDir.py
+++ DataProcessing/ProcessingScatsFromDir.py
@@ -1,17 +1,10 @@
-from pyspark import SparkContext
-from pyspark import SQLContext
 import os
 import ProcessScatFile
 
 def processScatsFiles(sqlContext, filteredTrafficLightsDf):
-    # sc = SparkContext()
-    # sqlContext = SQLContext(sc)
 
-    # traffic_lights_filepath = "hdfs://localhost:9000/test1/tlights_vic_4326.csv"
     volume_data_filepath = "E:\StudyNotes\Semester4\Project\data\scats2018"
-    newCsvPath = "E:\StudyNotes\Semester4\Project\data\scats2018_processedMonthFinal"
-
-    # trafficfile = sqlContext.read.csv(traffic_lights_filepath, header=True)
+    newCsvPath = "E:\StudyNotes\Semester4\Project\data\scats2018_processedMonthFullFinal"
 
     fileList = os.listdir(volume_data_filepath)
     concatenatedDf = 0
@@ -19,6 +12,7 @@ def processScatsFiles(sqlContext, filteredTrafficLightsDf):
         print(file)
         volumeFile = sqlContext.read.csv(volume_data_filepath + '/' + file, header=True)
         concatenatedDf = ProcessScatFile.calcNoOfTrafficPerHr(volumeFile, concatenatedDf, filteredTrafficLightsDf)
+
 
     finalProcessedDf = concatenatedDf.groupBy("NB_SCATS_SITE").avg('sum(sum_1)', 'sum(sum_2)', 'sum(sum_3)', 'sum(sum_4)',
                                                                    'sum(sum_5)', 'sum(sum_6)', 'sum(sum_7)', 'sum(sum_8)',
@@ -31,7 +25,20 @@ def processScatsFiles(sqlContext, filteredTrafficLightsDf):
                                                                    'sum(sum_21)', 'sum(sum_22)', 'sum(sum_23)',
                                                                    'sum(sum_24)')
 
+    finalProcessedDf = finalProcessedDf.join(filteredTrafficLightsDf, filteredTrafficLightsDf.SCAT_SITE_ID == finalProcessedDf.NB_SCATS_SITE, 'inner')
+    # # Combine wkt column from traffice file with the above dataframe
+    sumColumns = [i for i in finalProcessedDf.columns if i.startswith('avg(sum(sum_')]
+    sumColumns.append('NB_SCATS_SITE')
+    sumColumns.append('SCAT_SITE_NAME')
+    sumColumns.append('SCAT_LATITUDE')
+    sumColumns.append('SCAT_LONGITUDE')
+    finalProcessedDf = finalProcessedDf.select(sumColumns)
+    finalProcessedDf = finalProcessedDf.toDF('00:00 - 00:59', '01:00 - 01:59', '02:00 - 02:59', '03:00 - 03:59',
+                                '04:00 - 04:59', '05:00 - 05:59', '06:00 - 06:59', '07:00 - 07:59', '08:00 - 08:59',
+                                '09:00 - 09:59', '10:00 - 10:59', '11:00 - 11:59', '12:00 - 12:59', '13:00 - 13:59',
+                                '14:00 - 14:59', '15:00 - 15:59', '16:00 - 16:59', '17:00 - 17:59', '18:00 - 18:59',
+                                '19:00 - 19:59', '20:00 - 20:59', '21:00 - 21:59', '22:00 - 22:59', '23:00 - 23:59',
+                                'scatSiteId','scatSiteName', 'latitide', 'longitude')
     print(finalProcessedDf.count())
-    finalProcessedDf.show()
     finalProcessedDf.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").mode('append') \
         .save(newCsvPath)
