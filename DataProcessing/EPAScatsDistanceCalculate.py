5c90a9d91646b0c0cbd6c335adf4ca4191f3aad1
Sindhu-Sekar <ssekar@student.unimelb.edu.au>
2019-05-08 19:55:36 +1000

    Refactoring & changing structure of dataframe

--- DataProcessing/EPAScatsDistanceCalculate.py
+++ DataProcessing/EPAScatsDistanceCalculate.py
@@ -1,51 +1,36 @@
-import json
 import getDistance
-import ProcessingScatsFromDir
-from pyspark import SparkContext
-from pyspark import SQLContext
 
-sc = SparkContext()
-sqlContext = SQLContext(sc)
+def filterScatsDataWithinEPA(sc, sqlContext, trafficLightDataPath, EPAStationDataPath):
 
-traffic_lights_filepath = "hdfs://45.113.232.133:9000/pointData/tlights_vic_4326.csv"
+    datastore = sqlContext.read.json(EPAStationDataPath)
+    trafficfile = sqlContext.read.csv(trafficLightDataPath, header=True).collect()
+    finalList = []
+    for x in trafficfile:
+        newList = x["WKT"].split()
+        if len(newList) == 3:
+            lon = newList[1]
+            lon = lon[1:]
+            lat = newList[2]
+            lat = lat[:-1]
+            tempList = (x["SITE_NO"], x["SITE_NAME"], lat, lon, x["WKT"])
+            # Remove duplicate scats site ids (Some sites has 2 points for the same id eg:site_no  5877:
+            if not any(x["SITE_NO"] in row for row in finalList):
+                finalList.append(tempList)
 
-trafficfile = sqlContext.read.csv(traffic_lights_filepath, header=True)
-coordinateList = trafficfile.select("WKT", "SITE_NO", "SITE_NAME").collect()
-finalList = []
-for x in coordinateList:
-    newList = x["WKT"].split()
-    if len(newList) == 3:
-        lon = newList[1]
-        lon = lon[1:]
-        lat = newList[2]
-        lat = lat[:-1]
-        tempList = (x["SITE_NO"], x["SITE_NAME"], lat, lon)
-        # Remove duplicate scats site ids (Some sites has 2 points for the same id eg:site_no  5877:
-        if not any(x["SITE_NO"] in row for row in finalList):
-            finalList.append(tempList)
+    distanceDataList = []
+    for data in datastore.rdd.collect():
+        for trafficData in finalList:
+            try:
+                dist = getDistance.calculateDistance(float(data['Latitude']), float(data['Longitude']), float(trafficData[2]), float(trafficData[3]))
+                if dist <= 2.0:
+                    otherList = (trafficData[0], trafficData[1], trafficData[4])
 
-datastore = sqlContext.read.json("hdfs://45.113.232.133:9000/pointData/stationData.json")
+                    if not any(otherList[0] in row for row in distanceDataList):
+                        distanceDataList.append(otherList)
 
-distanceDataList = []
-for data in datastore.rdd.collect():
-    for trafficData in finalList:
-        try:
-            dist = getDistance.calculateDistance(float(data['Latitude']), float(data['Longitude']), float(trafficData[2]), float(trafficData[3]))
-            if dist <= 2.0:
-                otherList = (data['SiteId'], data['Name'], data['Latitude'], data['Longitude'],
-                             trafficData[0], trafficData[1], trafficData[2], trafficData[3])
+            except ValueError:
+                continue
 
-                if not any(otherList[5] in row for row in distanceDataList):
-                    distanceDataList.append(otherList)
-
-        except ValueError:
-            continue
-
-rdd = sc.parallelize(distanceDataList)
-filteredTrafficLightsDf = sqlContext.createDataFrame(rdd, ["EPA_SITE_ID", "EPA_SITE_NAME", "EPA_LATITUDE", "EPA_LONGITUDE",
-                                      "SCAT_SITE_ID", "SCAT_SITE_NAME", "SCAT_LATITUDE", "SCAT_LONGITUDE"])
-
-# filteredTrafficLightsDf.coalesce(1).write.format("com.databricks.spark.csv").option("header", "true").mode('append') \
-#         .save("E:/StudyNotes/Semester4/Project/data/finalfilteredData")
-
-ProcessingScatsFromDir.processScatsFiles(sqlContext, filteredTrafficLightsDf)
+    rdd = sc.parallelize(distanceDataList)
+    df = sqlContext.createDataFrame(rdd, ["SCAT_SITE_ID", "SCAT_SITE_NAME", "WKT"])
+    return df
