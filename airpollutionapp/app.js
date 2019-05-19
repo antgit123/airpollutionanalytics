f93c21cf42ac44834d4e056ca2d171bd828ef338
antgit123 <antariksh.maverick92@gmail.com>
2019-05-19 12:29:02 +1000

    Air Pollution App changes-adding map

--- airpollutionapp/app.js
+++ airpollutionapp/app.js
@@ -6,35 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var visualizationRouter = require('./routes/visualization');
 var app = express();
-
-// var webhdfsmanager = require('webhdfs');
-// var request = require("request");
-// let url = "http://45.113.232.133";
-// let port = 50070;
-// let dir_path = "/Processed2014";
-// let path = "/webhdfs/v1/" + dir_path + "?op=LISTSTATUS&user.name=hdfs";
-// let full_url = url + ':' + port + path;
-//
-// var hdfs_file_operations = {
-//     getFileData: (fileurl) => {
-//         request(full_url, (error, response, body) => {
-//             if (!error && response.statusCode === 200) {
-//                 let jsonStr = JSON.parse(body);
-//                 let fileObject = jsonStr.FileStatuses.FileStatus;
-//                 let objLength = Object.entries(fileObject).length;
-//                 console.log(fileObject[1]['pathSuffix']);
-//
-//                 console.log("Number of files in the folder: ", objLength);
-//             } else {
-//                 console.log("Error in retrieving files");
-//             }
-//         })
-//     }
-// }
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/visualization', visualizationRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
